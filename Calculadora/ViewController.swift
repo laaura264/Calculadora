d7b5ef0a676421acea22b6e9b7643d5b50d4ee82
laaura264 <32044280+laaura264@users.noreply.github.com>
2019-01-31 11:06:03 +0100

    init commit

---
+++ Calculadora/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Laura Arranz on 25/01/2019.
+//  Copyright © 2019 Laura Arranz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //quitar la barra de estado
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+
+
+}
+
